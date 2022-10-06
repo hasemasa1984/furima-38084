4614ef906edc6f82d1cb026fc69f6d6b86dd6075
hasemasa1984 <hasemasa0719@gmail.com>
2022-10-06 20:07:04 +0900

    修正6

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,10 +10,10 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname,:first_name,:last_name,:first_name_kana,:last_name_kana,:birth_date ])
   end
 
-  #def basic_auth
-    #authenticate_or_request_with_http_basic do |username, password|
-      #username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]  
-      #username == 'hasemasa' && password == '0719'
-    #end
-  #end
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]  
+      username == 'hasemasa' && password == '0719'
+    end
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,6 +7,9 @@ RSpec.describe User, type: :model do
 
   describe 'ユーザー新規登録' do
     context '新規登録できるとき' do
+      it '正常に登録できる' do
+        expect(@user).to be_valid
+      end
       it 'パスワードは、6文字以上での入力が必須であること' do
         @user.password = 'test1234'
         @user.password_confirmation = "test1234"
@@ -72,15 +75,40 @@ RSpec.describe User, type: :model do
       end
     end
     context '新規登録出来ない時' do
+      it 'パスワードは、6文字以上での入力が必須であること' do
+        @user.password = 'test1234'
+        @user.password_confirmation = "test1234"
+        expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
+      end
+      it 'ニックネームが必須であること' do
+        @user.nickname = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include(" Nickname can't be blank")
+      end
+      it 'パスワードが必須であること' do
+        @user.password = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password can't be blank")
+      end
+      it 'メールアドレスが必須であること' do
+        @user.email = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Email can't be blank")
+      end
+      it '生年月日が必須であること' do
+        @user.birth_date = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Birth date can't be blank")
+      end
       it '姓（全角）が空だと登録できない' do
         @user.first_name = ''
         @user.valid?
         expect(@user.errors.full_messages).to include("First name can't be blank")
       end
       it '姓（全角）に半角文字が含まれていると登録できない' do
-        @user.first_name = ''
+        @user.first_name = 'SSSSSs'
         @user.valid?
-        expect(@user.errors.full_messages).to include("First name can't be blank")
+        expect(@user.errors.full_messages).to include("First name 名字と名前がそれぞれ必須であること")
       end
       it '名（全角）が空だと登録できない' do
         @user.last_name = ''
@@ -108,24 +136,24 @@ RSpec.describe User, type: :model do
         expect(@user.errors.full_messages).to include("Last name kana can't be blank")
       end
       it '名（カナ）にカタカナ以外の文字（平仮名・漢字・英数字・記号）が含まれていると登録できない' do
-       @user.last_name_kana = ''
+       @user.last_name_kana = 'あ亜a1'
        @user.valid?
-       expect(@user.errors.full_messages).to include("Last name kana can't be blank")
+       expect(@user.errors.full_messages).to include("Last name kana is invalid")
       end
       it 'パスワードが英語のみでは登録できない' do
         @user.password = 'abcdef'
         @user.valid?
-        expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
+        expect(@user.errors.full_messages).to include("password is invalid")
       end
       it 'パスワードが数字のみでは登録できない' do
         @user.password = '123456'
         @user.valid?
-        expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
+        expect(@user.errors.full_messages).to include("password is invalid")
       end
       it "全角文字を含むパスワードでは登録できない" do
         @user.password = "AAAAAA"
         @user.valid?
-        expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
+        expect(@user.errors.full_messages).to include("password is invalid")
       end
     end
   end
