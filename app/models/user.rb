2261fe854cc08325d358922c8164eba43a552dde
hasemasa1984 <hasemasa0719@gmail.com>
2022-10-08 15:23:23 +0900

    名前の変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,7 +17,7 @@ class User < ApplicationRecord
 
         
         PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-         validates_format_of :password, with: VALID_PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
+         validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
         
 
          validates :first_name_kana,        presence: true, format: {with: /\A[ァ-ヶー－]+\z/ }     
