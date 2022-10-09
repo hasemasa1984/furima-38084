b88b96d7ab6fa8e95091aee04580799a0a4855e3
hasemasa1984 <hasemasa0719@gmail.com>
2022-10-09 17:05:48 +0900

    テストコードとFactorybot設定

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,19 @@
 FactoryBot.define do
   factory :item do
-    
+    item             { 'パソコン' }
+    explanation           { '3年前に購入したMacBookです' }
+    category_id           { 2 }
+    condition_id          { 2 }
+    delivery_charge_id      { 2 }
+    prefecture_id         { 2 }
+    scheduled_delivery_id      { 2 }
+    product_price                 { 50000 }
+
+    association :user
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/sample.jpeg'), filename: 'sample.jpeg')
+    end
   end
 end
+
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @item = FactoryBot.build(:item)
+  end
+
+  describe '商品新規登録' do
+    context '新規登録できるとき' do
+      it '正常に登録できる' do
+        expect(@item).to be_valid
+      end
+    end
+  end
 end
