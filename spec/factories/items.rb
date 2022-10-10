72593cd2bd8c1e41f6bd03b84b8336f3fc40dbba
hasemasa1984 <hasemasa0719@gmail.com>
2022-10-10 22:12:19 +0900

    正常系テストコードの実装

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,18 +1,18 @@
 FactoryBot.define do
   factory :item do
-    item             { 'パソコン' }
-    explanation           { '3年前に購入したMacBookです' }
+    title             { 'パソコン' }
+    products           { '3年前に購入したMacBookです' }
     category_id           { 2 }
     condition_id          { 2 }
     delivery_charge_id      { 2 }
     prefecture_id         { 2 }
     scheduled_delivery_id      { 2 }
-    product_price                 { 50000 }
+    product_price_id                 { 50000 }
 
     association :user
 
     after(:build) do |item|
-      item.image.attach(io: File.open('public/images/sample.jpeg'), filename: 'sample.jpeg')
+      item.image.attach(io: File.open('public/images/alejandro-escamilla-yC-Yzbqy7PY-unsplash.jpg'), filename: 'alejandro-escamilla-yC-Yzbqy7PY-unsplash.jpg')
     end
   end
 end
