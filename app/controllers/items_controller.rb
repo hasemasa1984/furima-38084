c436d0f664c28b381197f61e354c3da33adcd101
hasemasa1984 <hasemasa0719@gmail.com>
2022-10-12 20:47:07 +0900

    商品出品機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,9 +3,9 @@ class ItemsController < ApplicationController
   #before_action :set_item, only: [:edit, :update, :show, :destroy]
 
 
-  #def index
-    #@items = Item.all#.order("created_at DESC")
-  #end
+  def index
+    @items = Item.all.order("created_at DESC")
+  end
 
   def new
     @item = Item.new
