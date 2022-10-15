d47c2bffff1fe36f4fa6a1ffa26b826a8bddb54a
hasemasa1984 <hasemasa0719@gmail.com>
2022-10-15 18:58:39 +0900

    修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -39,7 +39,6 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    current_user.id == @item.user_id
     if current_user.id == @item.user_id
       @item.destroy
       redirect_to root_path
